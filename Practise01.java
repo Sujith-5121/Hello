8d845064bbcd976254abe918b16f599c5be7c8c7
Sujith-5121 <147568431+Sujith-5121@users.noreply.github.com>
2024-07-11 22:36:05 +0530

    Create Practise01.java

---
+++ Practise01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Practise01
+{
+	static Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	static Boolean b=s.nextBoolean();
+	public static void main(String[] args)
+	{
+		Practise01 obj=new Practise01();
+		System.out.println(obj.a);
+		System.out.println(b);
+	}
+}
